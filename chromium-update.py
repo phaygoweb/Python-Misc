b70795314360e1743cd5fc6aceba7d77f52ee989
Kyle Matheny <mathenk2+phaygoweb@gmail.com>
2011-05-03 10:12:08 -0700

    Installs latest Chromium build -Added a .log file

--- chromium-update.py
+++ chromium-update.py
@@ -25,7 +25,14 @@ def main():
   # Notify of a completed install
   print("Chromium updated to latest build.")
   
+  logging.basicConfig(filename="chromium-installs.log", level=logging.INFO)
+  localTime = time.asctime(time.localtime(time.time()))
+  logging.info('[' + localTime + '] Chromium ' + str(versionNumber) + ' installed')
+  
 if __name__ == '__main__':
   import urllib.request
   import os
+  import logging
+  import time
+  
   main()
