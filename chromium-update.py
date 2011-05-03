207bf8be670a4785b84af8eb621ff20808352747
Kyle Matheny <mathenk2+phaygo@gmail.com>
2011-05-03 00:39:23 -0400

    Installs latest Chromium build

---
+++ chromium-update.py
@@ -0,0 +1,32 @@
+#! c:/python32/python.exe
+
+import urllib.request
+import os
+
+def main():
+  # Root URL to build directory
+  rootURL = "http://build.chromium.org/f/chromium/snapshots/chromium-rel-xp/"
+  
+  # Get latest build number
+  data = urllib.request.urlopen(rootURL+"LATEST").read()
+  versionNumber = int(data)
+  fileMode = str(data)[0]
+  
+  # Download the latest build
+  print("downloading Chromium build", str(versionNumber) + "...")
+  chromiumInstaller = urllib.request.urlopen(rootURL+str(versionNumber)+"/mini_installer.exe")
+
+  # Write out installer file
+  localFile = open("chromium_"+str(versionNumber)+".exe", "w" + fileMode)
+  localFile.write(chromiumInstaller.read())
+  localFile.close()
+
+  # Install
+  print("Installing...")
+  os.system("chromium_"+str(versionNumber)+".exe")
+
+  # Notify of a completed install
+  print("Chromium updated to latest build.")
+  
+if __name__ == '__main__':
+  main()
